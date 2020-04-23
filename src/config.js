ee6047292b8f92aa6d46473dde0a2e74fd1b1245
Victor Guerra Veloso <victorgvbh@gmail.com>
2020-04-22 21:48:21 -0300

    Added DB_USER and DB_PSK env vars for db authentication

--- src/config.js
+++ src/config.js
@@ -4,10 +4,13 @@ const PORT = process.env.PORT || 3000
 const DB_HOST = process.env.DB_HOST || "localhost"
 const DB_PORT = process.env.DB_PORT || 27017
 const DB_NAME = process.env.DB_NAME || "brazilian-addon-db"
-
+const DB_USER = process.env.DB_USER
+const DB_PSK = process.env.DB_PSK
 async function connect() {
     try {
-        await mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+        let CREDENTIALS = ""
+        if (DB_USER && DB_PSK) CREDENTIALS = `${DB_USER}:${DB_PSK}@`
+        await mongoose.connect(`mongodb://${CREDENTIALS}${DB_HOST}:${DB_PORT}/${DB_NAME}`)
     } catch (err) {
         throw new Error(`Could not connect to db 'mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}': ${err}`)
     }
