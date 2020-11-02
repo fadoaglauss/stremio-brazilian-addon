d4be117c9fe446224684345cd0d321192d794520
Fadoa Glauss <fadoa.glauss@gmail.com>
2020-11-02 12:15:02 -0300

    Update mongodb port

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 
+const PORT = process.env.PORT || 3000
 const DB_HOST = process.env.DB_HOST || "localhost"
 const DB_PORT = process.env.DB_PORT || 27017
 const DB_NAME = process.env.DB_NAME || "brazilian-addon-db"
