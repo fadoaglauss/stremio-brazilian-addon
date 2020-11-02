79879467d905813308fcc9bd4bbbef54d6c55015
Fadoa Glauss <fadoa.glauss@gmail.com>
2020-11-02 12:20:27 -0300

    Update mongodb port

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000
 const DB_HOST = process.env.DB_HOST || "localhost"
+const DB_PORT = process.env.DB_PORT || 27017
 const DB_NAME = process.env.DB_NAME || "brazilian-addon-db"
 const DB_USER = process.env.DB_USER
 const DB_PSK = process.env.DB_PSK
@@ -10,11 +11,11 @@ async function connect() {
     try {
         let CREDENTIALS = ""
         if (DB_USER && DB_PSK) CREDENTIALS = `${DB_USER}:${DB_PSK}@`
-        const mongouri = `mongodb+srv://${CREDENTIALS}${DB_HOST}/${DB_NAME}`
+        const mongouri = `mongodb+srv://${CREDENTIALS}${DB_HOST}:/${DB_NAME}`
         await mongoose.connect(mongouri)
         return mongouri
     } catch (err) {
-        throw new Error(`Could not connect to db 'mongodb://${DB_HOST}/${DB_NAME}': ${err}`)
+        throw new Error(`Could not connect to db 'mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}': ${err}`)
     }
 }
 
